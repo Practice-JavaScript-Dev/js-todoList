d8c342541c30104d9ddc36459c6b38b0792eba60
James <jamesnorbertowork@gmail.com>
2021-01-01 09:58:46 +0900

    save

---
+++ main.js
@@ -0,0 +1,5 @@
+// CREATE VARIABLES TO MANIPULATE ELEMENTS IN CSS CLASSES ...
+const myForm = document.querySelector('.add-tasks');
+let myList = document.querySelector('.todos')
+
+// CREATING A TASK
