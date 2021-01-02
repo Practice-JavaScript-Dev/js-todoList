d1836f08bb2edcfd51204a54299ffaec3c817ab9
James <jamesnorbertowork@gmail.com>
2021-01-02 09:04:14 +0900

    Struggling with javascript functions ... Will be back.

--- main.js
+++ main.js
@@ -8,10 +8,34 @@ let myList = document.querySelector('.todos')
 
 
 /******************************
-CREATE A TASK
+CREATING TASKS
 ******************************/
 
+/********************************************************************************
+
+GOT STUCK BELOW. I WILL STUDY GENERAL JAVASCRIPT FUNCTIONS AND RETURN LATER.
+CODEPEN LINKS FOR THE PROJECT: 
+
+https://codepen.io/bobaloushqee/pen/rNMJRYr?editors=0010
+
+
+***************　THIS CODEPEN IS EASIER FOR ME TO UNDERSTAND:　***************
+https://codepen.io/karangaba/pen/bGErXMG?editors=0010
+
+
+
 // Ability to add to the list
-const create = function() {
+function createTask(myTask) {
     list.innerHTML += <li class="list-item"></li>
 }
+
+
+
+// The 'submit' event fires when a <form> is submmitted
+myForm.addEventListener('submit', anotherFunction) {
+    anotherFunction.preventDefault() // Event.preventDefault() method --> "if the event does not get explicitly handled, its default action should not be taken as it normally would be.""
+    
+    // 
+
+}
+/********************************************************************************
